4f6d7fda6626650044fd51f99189ab507736a867
王來遠 <sinosoft@sinosoft.hk>
2021-03-24 16:19:17 +0800

    lock 多线程售票 lambda表达式创建线程

---
+++ src/main/java/com/wly/atguigu/jucandjvm/SaleTicketDemo01.java
@@ -0,0 +1,34 @@
+package com.wly.atguigu.jucandjvm;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class Ticket{
+    private int number =30;
+    Lock lock=new ReentrantLock();
+    public void sale(){
+        try{
+            lock.lock();
+            if(number>0){
+                System.out.println(Thread.currentThread().getName()+"售出了第："+number--+"票，还剩："+number+"张票");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            lock.unlock();
+        }
+    }
+}
+
+/**
+ * 题目：三个售票员 买出 30张票
+ */
+public class SaleTicketDemo01 {
+    public static void main(String[] args) {
+        Ticket ticket=new Ticket();
+        new Thread(() ->{for(int i=0;i<=40;i++) ticket.sale();},"A").start();
+        new Thread(() ->{for(int i=0;i<=40;i++) ticket.sale();},"B").start();
+        new Thread(() ->{for(int i=0;i<=40;i++) ticket.sale();},"C").start();
+    }
+
+}
