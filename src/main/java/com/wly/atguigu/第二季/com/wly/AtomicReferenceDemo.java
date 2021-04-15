66c478f8990ea3fce20ee018d8b073429d43e2c2
wly <1157810116@qq.com>
2021-04-15 14:48:58 +0800

    引用原子类 及带时间戳的引用原子类解决ABA问题

---
+++ src/main/java/com/wly/atguigu/第二季/com/wly/AtomicReferenceDemo.java
@@ -0,0 +1,26 @@
+package com.wly.atguigu.第二季.com.wly;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+@Getter
+@ToString
+@AllArgsConstructor
+class User{
+    String userName;
+    int age;
+}
+public class AtomicReferenceDemo {
+
+
+    public static void main(String[] args) {
+        User z3 = new User("z3", 22);
+        User li4 = new User("li4", 22);
+        AtomicReference<User> atomicReference=new AtomicReference<>();
+        atomicReference.set(z3);
+        System.out.println(atomicReference.compareAndSet(z3,li4)+"\t User:"+ atomicReference.get().getUserName());
+    }
+}
